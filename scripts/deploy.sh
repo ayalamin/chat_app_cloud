c5e90a5479df8d698c4b5ee67ee73574465f7334
Ayala Min Hahar <ayala59020@gmail.com>
2023-09-27 09:52:51 +0300

    ayalaaa

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,20 +3,13 @@
 # Function to handle errors
 
 handle_error() {
-
   echo "Error: $1"
-
   exit 1
-
 }
 
-# Get version from user
+version=$1
+commit_hash=$2
 
-read -p "Enter version: " version
-
-# Get commit hash from user
-
-read -p "Enter commit hash: " commit_hash
 
 # Check if image exists
 
@@ -40,32 +33,32 @@ if docker image inspect chat-app:$version >/dev/null 2>&1; then
 
 fi
 
-# Tag the commit
-
 git tag "$version" "$commit_hash" || handle_error "Failed to tag the commit"
-
-# Build the image
 
 docker build -t chat-app:$version . || handle_error "Failed to build the image"
 
-# Push the tag to GitHub repository
-
 git push origin "$version" || handle_error "Failed to push to GitHub"
 
-# Success message
-
-echo "Deployment successful!"
 # Ask user if they want to push the image to Artifact Registry
 read -p "Do you want to push the image to Artifact Registry? (y/n): " push_image
 
 if [ "$push_image" == "y" ]; then
   # Authenticate with service account impersonation
+  appname="chat-app"
+  # Push the Docker image to Artifact Registry (optional)
+  echo "Pushing Docker image to artifactregistry"
+  gcloud config set auth/impersonate_service_account artifact-admin-sa@grunitech-mid-project.iam.gserviceaccount.com  
+  gcloud auth configure-docker me-west1-docker.pkg.dev
+  # artifact_registry_image=me-west1-docker.pkg.dev/grunitech-mid-project/gittyrabinowitz-chat-app-images/${appname}:${requestedVersion}
+  # docker tag ${image_name} ${artifact_registry_image} 
+  # docker push ${artifact_registry_image}
+  gcloud config set auth/impersonate_service_account gitty-instance-SA@grunitech-mid-project.iam.gserviceaccount.com
   gcloud auth activate-service-account --key-file=/path/to/service-account-key.json --impersonate-service-account=artifact-admin-sa@grunitech-mid-project.iam.gserviceaccount.com
 
   # Push the image to Artifact Registry
-  gcloud artifacts docker images push gcr.io/Grunitech Mid Project/chat_app_cloud:$version
+  # gcloud artifacts docker images push gcr.io/Grunitech Mid Project/chat_app_cloud:$version
 
-  echo "Image pushed to Artifact Registry."
+  # echo "Image pushed to Artifact Registry."
 else
   echo "Skipping image push to Artifact Registry."
 fi
