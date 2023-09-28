a8a69a6841beb573c40d00d5efa8d42f51341228
Ayala Min Hahar <ayala59020@gmail.com>
2023-09-28 18:45:38 +0300

    aaa

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -43,20 +43,15 @@ git push origin "$version" || handle_error "Failed to push to GitHub"
 read -p "Do you want to push the image to Artifact Registry? (y/n): " push_image
 
 if [ "$push_image" == "y" ]; then
-  # Authenticate with service account impersonation
-  appname="chat-app"
-  # Push the Docker image to Artifact Registry (optional)
-  echo "Pushing Docker image to artifactregistry"
-  gcloud config set auth/impersonate_service_account artifact-admin-sa@grunitech-mid-project.iam.gserviceaccount.com  
-  gcloud auth configure-docker me-west1-docker.pkg.dev
-  # artifact_registry_image=me-west1-docker.pkg.dev/grunitech-mid-project/gittyrabinowitz-chat-app-images/${appname}:${requestedVersion}
-  # docker tag ${image_name} ${artifact_registry_image} 
-  # docker push ${artifact_registry_image}
-  gcloud config set auth/impersonate_service_account gitty-instance-SA@grunitech-mid-project.iam.gserviceaccount.com
-  gcloud auth activate-service-account --key-file=/path/to/service-account-key.json --impersonate-service-account=artifact-admin-sa@grunitech-mid-project.iam.gserviceaccount.com
 
-  # Push the image to Artifact Registry
-  # gcloud artifacts docker images push gcr.io/Grunitech Mid Project/chat_app_cloud:$version
+# Authenticate with service account impersonation
+gcloud auth activate-service-account --key-file=path/to/artifact-admin-sa-key.json --impersonate-service-account=artifact-admin-sa@grunitech-mid-project.iam.gserviceaccount.com
+
+gcloud auth configure-docker me-west1-docker.pkg.dev
+
+docker tag chat-app:$version me-west1-docker.pkg.dev/grunitech-mid-project/ayala-chat-app-images/chat-app:$version
+docker push me-west1-docker.pkg.dev/grunitech-mid-project/ayala-chat-app-images/chat-app:$version
+
 
   # echo "Image pushed to Artifact Registry."
 else
